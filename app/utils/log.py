521c6313f3ffeca2f528a73fa37915680e88047c
littletom123 <1148238139@qq.com>
2023-02-02 23:01:05 +0800

    增加日志模块

---
+++ app/utils/log.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+@File: log.py
+@Time: 2023/2/2-22:32
+@Author: Li Dongchao
+@Desc: 
+"""
+import os
+
+from app.config import setting
+
+from loguru import logger
+
+logger.add(os.path.join(setting.log_path, "{time:YYYY-MM-DD}.log"))
+
+
+if __name__ == '__main__':
+    logger.info("日志测试")
