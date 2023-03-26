1c632a627f209cfa3eba466f875a4b8294cc8fcb
littletom123 <1148238139@qq.com>
2023-03-26 10:54:12 +0800

    注册新增加的功能

--- app/manager/manager.py
+++ app/manager/manager.py
@@ -7,9 +7,10 @@
 @Desc: 
 @release: 
 """
+import datetime
 from queue import Queue
 from threading import Thread
-from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 from app.fetcher.fetcher import Fetcher
 from app.schemas.proxy import Proxy
@@ -17,6 +18,7 @@ from app.validator.validator import Validator
 from app.db.redis_client import RedisClient
 from app.utils import logger
 from app.config import setting
+from app.exception import AllValidError
 
 
 class Manager(object):
@@ -31,11 +33,18 @@ class Manager(object):
         """
         从各个免费代理网站获取代理
         """
-        # todo: 后续多个不同的免费代理网站如何进行设计？尽量设置为可插拔式
-        proxies = self.fetcher.execute()
-        while proxies:
-            cur_proxy = proxies.pop()
-            self.db.add(proxy=cur_proxy)
+        for cur_proxy in self.fetcher.get():
+            # 如果超过预期代理数量上限，删除分数最低的
+            # 如果全部都是有效的了，就放弃本次新代理的入库
+            if self.db.all() >= setting.max_proxy_limit:
+                try:
+                    last = self.db.last()
+                    self.db.remove(last)
+                    self.db.add(proxy=cur_proxy)
+                except AllValidError as _:
+                    self.save()
+                    return
+        self.save()
 
     def validate(self, proxy: Proxy):
         """
@@ -49,9 +58,10 @@ class Manager(object):
             self.db.decrease(proxy)
 
     def validate_all(self):
+        """多线程验证数据库中的代理"""
         all_proxies = self.db.all()
         self.logger.info(f"当前数据库中有{self.db.count()}条数据，即将开始验证代理有效性...")
-        q = Queue()
+        q = Queue()  # 使用Queue实现多线程间的数据共享
         [q.put(proxy) for proxy in all_proxies]
         while not q.empty():
             thread_list = []
@@ -63,6 +73,7 @@ class Manager(object):
                 t.start()
             for t in thread_list:
                 t.join()
+        self.db.save()
         self.logger.info(f"已验证{self.db.count()}条代理数据，"
                          f"其中有效代理{self.db.count_max()}条")
 
@@ -73,20 +84,44 @@ class Manager(object):
         """
         return self.db.get().string()
 
+    def delete_proxy(self, proxy_str: str):
+        """删除代理"""
+        proxy = Proxy.str2proxy(proxy_str)
+        self.db.remove(proxy)
+
+    def quantity_all(self):
+        """查询代理总数"""
+        return self.db.count()
+
+    def quantity_valid(self):
+        """查询有效代理总数"""
+        return self.db.count_max()
+
     def save(self):
         """数据持久化"""
         self.db.save()
 
     def add_fetch_scheduler(self):
         """创建抓取代理ip定时任务"""
-        scheduler = BackgroundScheduler()
-        scheduler.add_job(self.fetch, trigger='interval', hours=setting.fetch_interval)
+        scheduler = BlockingScheduler()
+        scheduler.add_job(
+            self.fetch,
+            trigger='interval',
+            hours=setting.fetch_interval,
+            next_run_time=datetime.datetime.now()
+        )
         scheduler.start()
 
     def add_validate_scheduler(self):
         """创建验证代理ip定时任务"""
-        scheduler = BackgroundScheduler()
-        scheduler.add_job(self.validate_all, trigger='interval', hours=setting.validate_interval)
+        scheduler = BlockingScheduler()
+        scheduler.add_job(
+            self.validate_all,
+            trigger='interval',
+            hours=setting.validate_interval,
+            next_run_time=datetime.datetime.now()
+
+        )
         scheduler.start()
 
 
