aa887ac02c9c46544d7a711378b16468b920949a
littletom123 <1148238139@qq.com>
2023-03-26 10:43:19 +0800

    server完善

--- app/server/server.py
+++ app/server/server.py
@@ -20,7 +20,7 @@ manager = Manager()
 
 @app.get("/")
 def index():
-    return "ProxyPoll"
+    return "ProxyPool"
 
 
 @app.get("/get", summary="获取代理")
@@ -29,12 +29,31 @@ def get():
         proxy = manager.get_proxy()
         return proxy
     except EmptyPoolError as _:
-        return "No proxy in pool now, please check whether the fetcher is work"
+        return "No proxy in pool."
+
+
+@app.delete("/delete", summary="删除代理")
+def delete(proxy: str):
+    try:
+        manager.delete_proxy(proxy)
+        return "OK"
+    except ValueError as _:
+        return "Proxy format error."
+
+
+@app.get("/quantity/all", summary="代理总数")
+def quantity_all():
+    return manager.quantity_all()
+
+
+@app.get("/quantity/valid", summary="有效代理数")
+def quantity_valid():
+    return manager.quantity_valid()
 
 
 def start_server():
     import uvicorn
-    uvicorn.run("app.server.server:app", host="127.0.0.1", port=8000)
+    uvicorn.run("app.server.server:app", host="0.0.0.0", port=8000)
 
 
 if __name__ == '__main__':
